53e3de04408a257a1b84a1a2a6b796a421acb88b
Ahmed Adan <ahmed.adan@gmail.com>
2026-01-24 03:33:07 +0000

    goose-linux 1.21.1

--- Casks/goose-linux.rb
+++ Casks/goose-linux.rb
@@ -1,6 +1,6 @@
 cask "goose-linux" do
-  version "1.18.0"
-  sha256 "09decb679d4a7de9367f02450d93e7717de8a3b4bb15f3909574bcb4b41fdcd5"
+  version "1.21.1"
+  sha256 "a2eb6b26fa9495abbf74cf7b0a2b600f7f80cb4ccb9ab61ab44de56d82958501"
 
   url "https://github.com/block/goose/releases/download/v#{version}/Goose-#{version}-1.x86_64.rpm",
       verified: "github.com/block/goose/"
