901f941da43b3aa087f905c258221e05996f0ed6
Ahmed Adan <ahmed.adan@gmail.com>
2026-05-15 03:04:39 -0400

    bcvk 0.17.0 (#360)

--- Formula/bcvk.rb
+++ Formula/bcvk.rb
@@ -1,7 +1,7 @@
 class Bcvk < Formula
   desc "CLI tool for launching ephemeral VMs from bootc container images"
   homepage "https://github.com/bootc-dev/bcvk"
-  url "https://github.com/bootc-dev/bcvk.git", tag: "v0.15.0", revision: "b32b57d7bced201a66a1ba95b8739fb8310e109b"
+  url "https://github.com/bootc-dev/bcvk.git", tag: "v0.17.0", revision: "b807a2dcba034ab8cbe97f7ed94ba9d5fe6fa749"
   license any_of: ["Apache-2.0", "MIT"]
 
   livecheck do
